1a60110999312dd1f81085848b43b8d69257c912
FarlinaBarik <57106527+FarlinaBarik@users.noreply.github.com>
2019-12-17 16:41:38 +0600

    Index file and register file(php and html)

---
+++ Index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Bookish</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello World!</p>";
+        ?>
+        <a href="Login.php"> Click here to login</a><br/>
+        <a href="Register.php"> Click here to register </a>
+	  
+	
+    </body>
+</html> 
